1cad3c611e26274e806386ab42ae27594646fa50
alouilouai <louaialoui1993@gmail.com>
2025-01-28 22:10:44 +0100

    folder setup + fixing the LF end of line fro prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+// Call and await the bootstrap function here
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+  process.exit(1);
+});
