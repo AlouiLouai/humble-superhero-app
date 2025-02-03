3dc071cd5e0539d3940c369c5435435c38313297
alouilouai <louaialoui1993@gmail.com>
2025-02-03 19:10:49 +0100

    start using the prisma logic

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SuperheroesModule } from '../superheroes/superheroes.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { SuperheroesModule } from '../superheroes/superheroes.module';
       envFilePath: '.env',
       isGlobal: true,
     }),
+    PrismaModule,
     SuperheroesModule,
   ],
   controllers: [AppController],
