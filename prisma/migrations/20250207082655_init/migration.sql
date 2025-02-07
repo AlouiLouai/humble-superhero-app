576be0ebf1f1259dffcfcfbacdb79bc548432155
alouilouai <louaialoui1993@gmail.com>
2025-02-07 09:27:32 +0100

    migrated prisma schema

---
+++ prisma/migrations/20250207082655_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Superhero" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "superpower" TEXT NOT NULL,
+    "humilityScore" INTEGER NOT NULL,
+
+    CONSTRAINT "Superhero_pkey" PRIMARY KEY ("id")
+);
